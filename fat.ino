961706d9a7a1dff61abb67510927ae3bcb429420
disaster37 <linuxworkgroup@hotmail.com>
2020-10-14 21:40:49 +0200

    Add water, air and himidity captor

--- fat.ino
+++ fat.ino
@@ -1,14 +1,23 @@
+#include "settings.h"
+#include "timer.h"
 #include <Firmata.h>
 #include "utility/firmataDebug.h"
 #include <avr/wdt.h>
 #include <aREST.h>
+#include <DS18B20.h>
+#include "DHTesp.h"
 
 //#define SERIAL_DEBUG
 
 // Create aREST instance
 aREST rest = aREST();
 bool isRebooted = false;
-
+DS18B20 ds(PIN_DS18B20);
+DHTesp dht = DHTesp();
+int waterTemp = 0;
+int airTemp = 0;
+int airHumidity = 0;
+Timer timer;
 
 
 void setup()
@@ -21,8 +30,17 @@ void setup()
   rest.set_id("000004");
   rest.set_name("DFP");
   rest.variable("isRebooted",&isRebooted);
+  rest.variable("waterTemp", &waterTemp);
+  rest.variable("airTemp", &airTemp);
+  rest.variable("airHumidity", &airHumidity);
 
   rest.function("acknoledgeReboot", acknoledgeReboot);
+
+  timer = Timer();
+  timer.setTimerInSecond(10);
+  timer.start();
+  dht.setup(PIN_DHT, DHTesp::DHT11);
+  readCaptor();
 
 
   // Start watchdog
@@ -32,7 +50,11 @@ void setup()
  
 void loop()
 {
-
+  timer.update();
+  if(timer.isJustFinished()) {
+    readCaptor();
+    timer.start();
+  }
   rest.handle(Serial);
   wdt_reset();
     
@@ -43,4 +65,16 @@ int acknoledgeReboot(String command) {
   DEBUG_PRINTLN("Acknoledge reboot");
   isRebooted = false;
   return 1;
+}
+
+void readCaptor() {
+  waterTemp = ds.getTempC();
+  airTemp = (int)dht.getTemperature();
+  airHumidity = (int)dht.getHumidity();
+  DEBUG_PRINT("WaterTemp:");
+  DEBUG_PRINTLN(waterTemp);
+  DEBUG_PRINT("AirTemp");
+  DEBUG_PRINTLN(airTemp);
+  DEBUG_PRINT("AirHumidity");
+  DEBUG_PRINTLN(airHumidity);
 }
---
+++ settings.h
@@ -0,0 +1,23 @@
+
+#ifndef Settings_h
+#define Settings_h
+
+// Pin for LCD
+static const uint8_t PIN_LCD_RS = 31;
+static const uint8_t PIN_LCD_ENABLE = 33;
+static const uint8_t PIN_LCD_D0 = 22;
+static const uint8_t PIN_LCD_D1 = 23;
+static const uint8_t PIN_LCD_D2 = 24;
+static const uint8_t PIN_LCD_D3 = 25;
+static const uint8_t PIN_LCD_D4 = 26;
+static const uint8_t PIN_LCD_D5 = 27;
+static const uint8_t PIN_LCD_D6 = 28;
+static const uint8_t PIN_LCD_D7 = 29;
+static const uint8_t PIN_LCD_CONTRAST = 32;
+static const uint8_t PIN_LCD_LED = 30;
+
+// Pin for tempeture / humidity
+static const uint8_t PIN_DHT = 6;
+static const uint8_t PIN_DS18B20 = 7;
+
+#endif
